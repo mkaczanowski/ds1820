14cf08b0c5a40d806fab625d912bfbf21b00a4cc
Kaczanowski Mateusz <kaczanowski.mateusz@gmail.com>
2020-07-05 14:17:44 +0200

    stylecheck

--- static/js/custom.js
+++ static/js/custom.js
@@ -6,14 +6,14 @@ $(document).on("pageinit",function() {
 });
 
 function reload() { 
-	var settings = getSettings();
-	settings.sensors.forEach(function(item, index) {
-		getSensorInfo(settings, item, 5);
-	});
+    var settings = getSettings();
+    settings.sensors.forEach(function(item, index) {
+        getSensorInfo(settings, item, 5);
+    });
 }
 
 function normalizeSensorName(name) {
-	return name.split("/")[5].replace("-", "_");
+    return name.split("/")[5].replace("-", "_");
 }
 
 function getSensorInfo(settings, sensor, limit){
@@ -22,45 +22,46 @@ function getSensorInfo(settings, sensor, limit){
 
     $.ajax({
         type: "POST",
-        url: "http://localhost:8888/get_temp",
+        url: "http://" + window.location.host + "/get_temp",
         data : {"count" : limit, "sensor": source_sensor},
         dataType : "json",
         success: function(data) {
-		var history = [];
-		var elem = `
-			<li id="li_{sensor}" data-cards-type='weather'>
-				<h1>Sensor: {sensor}</h1>
-				<ul class='detail'>
-					<li><i id='sensor_{sensor}' class='icon-circle {icon}'></i>
-						<span id='status_sensor_{sensor}'>{sensor_status}</span>
-					</li>
-					<li id='sensor_{sensor}_temp'>{sensor_temp}&deg;</li>
-				</ul>
-				<ul class='week' id='sensor_{sensor}_history'>
-					{sensor_data}
-				</ul>
-			</li>`;
+            var history = [];
+            var elem = `
+            <li id="li_{sensor}" data-cards-type='weather'>
+                <h1>Sensor: {sensor}</h1>
+                <ul class='detail'>
+                    <li><i id='sensor_{sensor}' class='icon-circle {icon}'></i>
+                        <span id='status_sensor_{sensor}'>{sensor_status}</span>
+                    </li>
+                    <li id='sensor_{sensor}_temp'>{sensor_temp}{symbol}</li>
+                </ul>
+                <ul class='week' id='sensor_{sensor}_history'>
+                    {sensor_data}
+                </ul>
+            </li>`;
 
-		var online = (Math.round(+new Date()/1000) - data[0].timestamp) < settings.db_update_rate_secs + refresh_rate_secs;
+            var online = (Math.round(+new Date()/1000) - data[0].timestamp) < settings.db_update_rate_secs + refresh_rate_secs;
 
-            	$.each(data, function(i){
-            	     if(i == 0) return;
-            	     var date = new Date(data[i].timestamp*1000);
-            	     history.push("<li><span>"+date.getHours()+":"+date.getMinutes()+"</span><strong>"+data[i].temperature+"&deg;</strong></li>");
-	    	});
+            $.each(data, function(i){
+                if(i == 0) return;
+                var date = new Date(data[i].timestamp*1000);
+                history.push("<li><span>"+date.getHours()+":"+date.getMinutes()+"</span><strong>"+data[i].temperature+"&deg;</strong></li>");
+            });
 
-		var rendered = elem
-		    .replace(/\{sensor\}/g, normalized_sensor)
-		    .replace(/\{sensor_temp\}/g, data[0].temperature)
-		    .replace(/\{sensor_data\}/g, history.join(" "))
-		    .replace(/\{icon\}/g, online ? "sunny" : "cold")
-		    .replace(/\{sensor_status\}/g, "STATUS: " + (online ? "ON" : "OFF"));
+            var rendered = elem
+                .replace(/\{sensor\}/g, normalized_sensor)
+                .replace(/\{sensor_temp\}/g, data[0].temperature)
+                .replace(/\{sensor_data\}/g, history.join(" "))
+                .replace(/\{symbol\}/g, settings.temperature_scale == 0 ? "&deg;" : "&#8457;")
+                .replace(/\{icon\}/g, online ? "sunny" : "cold")
+                .replace(/\{sensor_status\}/g, "STATUS: " + (online ? "ON" : "OFF"));
 
-		if ($("#li_" + normalized_sensor).length == 1) {
-			$('#content ul #li_' + normalized_sensor).html(rendered);
-		} else {
-			$("#content ul").append(rendered);
-		}
+            if ($("#li_" + normalized_sensor).length == 1) {
+                $('#content ul #li_' + normalized_sensor).html(rendered);
+            } else {
+                $("#content ul").append(rendered);
+            }
         }
     }); 
 }
@@ -70,12 +71,12 @@ function getSettings(){
 
     $.ajax({
         type: "POST",
-        url: "http://localhost:8888/get_settings",
+        url: "http://" + window.location.host + "/get_settings",
         data : {"t": "test"},
         dataType : "json",
-	async: false,
+        async: false,
         success: function(data) {   
-		json_data = data;
+            json_data = data;
         }
     }); 
 
--- static/js/customForms.js
+++ static/js/customForms.js
@@ -3,7 +3,7 @@ $(document).on("pageinit",function() {
 
     $.ajax({
         type: "POST",
-        url: "http://localhost:8888/get_settings",
+        url: "http://" + window.location.host + "/get_settings",
         data : {"type" : "all"},
         dataType : "json",
         success: function(data) {
@@ -15,37 +15,37 @@ $(document).on("pageinit",function() {
 
             $("#range1a").val(data.sensors_range.min);
             $("#range1b").val(data.sensors_range.max).slider('refresh');
-            
+
             $("#range1c").val(data.email_send_rate_secs).slider('refresh');
             $("#range1d").val(data.db_update_rate_secs).slider('refresh');
 
-	    settings = data;
+            settings = data;
         }
     }); 
 
-    
+
     $("#settings").submit(function() {
-    	var post = {};
-	settings.host = $("#host").val();
-	settings.port = parseInt($("#port").val());
-	settings.email_recipient = $("#recipient").val();
-	settings.temperature_scale = parseInt($("#scale").val());
-	settings.sensors_range.min = parseInt($("#range1a").val());
-	settings.sensors_range.max = parseInt($("#range1b").val());
-	settings.email_send_rate_secs = parseInt($("#range1c").val());
-	settings.db_update_rate_secs = parseInt($("#range1d").val());
+        var post = {};
+        settings.host = $("#host").val();
+        settings.port = parseInt($("#port").val());
+        settings.email_recipient = $("#recipient").val();
+        settings.temperature_scale = parseInt($("#scale").val());
+        settings.sensors_range.min = parseInt($("#range1a").val());
+        settings.sensors_range.max = parseInt($("#range1b").val());
+        settings.email_send_rate_secs = parseInt($("#range1c").val());
+        settings.db_update_rate_secs = parseInt($("#range1d").val());
 
-    	$.ajax({
-    	       type: "POST",
-    	       url: "http://localhost:8888/set_settings",
-    	       data: {"data": JSON.stringify(settings)},
-    	       dataType : "json",
-    	       success: function(data)
-    	       {
-    	           
-    	       }
-    	     });
+        $.ajax({
+            type: "POST",
+            url: "http://" + window.location.host + "/set_settings",
+            data: {"data": JSON.stringify(settings)},
+            dataType : "json",
+            success: function(data)
+            {
+                window.location.href = "/index.html";
+            }
+        });
 
-    	return false; // avoid to execute the actual submit of the form.
+        return false; // avoid to execute the actual submit of the form.
     });
 });
